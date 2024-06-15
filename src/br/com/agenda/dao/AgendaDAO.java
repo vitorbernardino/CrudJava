47c73b02acabf54388e160584f43de7d23ec423f
Vitor Bernardino <vitor.hernandorena@hotmail.com>
2024-06-14 21:00:40 -0300

    getting items from db feature

--- src/br/com/agenda/dao/AgendaDAO.java
+++ src/br/com/agenda/dao/AgendaDAO.java
@@ -6,6 +6,9 @@ import br.com.agenda.model.Contato;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AgendaDAO {
 
@@ -41,5 +44,41 @@ public class AgendaDAO {
         }
     }
 
+    public List<Contato> getContatos(){
+        List<Contato> contatos = new ArrayList<Contato>();
+        String sql = "select * from contatos";
+        Connection conn = null;
+        PreparedStatement pstm = null;
+        ResultSet rs = null;
 
+        try{
+            conn = ConnectionFactory.getConnection();
+            pstm = (PreparedStatement) conn.prepareStatement(sql);
+            rs = pstm.executeQuery();
+
+            while (rs.next()){
+                Contato contato = new Contato();
+                contato.setId(rs.getInt("id"));
+                contato.setName(rs.getString("name"));
+                contato.setIdade(rs.getInt("idade"));
+                contato.setData(rs.getDate("dataCadastro"));
+                contatos.add(contato);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if(pstm != null){
+                    pstm.close();
+                }
+                if (conn != null){
+                    conn.close();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return contatos;
+    }
 }
